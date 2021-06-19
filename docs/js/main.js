7116dc7d99cefa83773e15afe7caf6be019b4edd
feudal <calmis87@gmail.com>
2021-06-19 18:27:12 +0300

    forget build

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,42 @@
 $(document).ready(function() {
+  //----------------------------------------slider--------------------------------------//
+  $('.brand-slider').slick({
+    autoplay: true,
+    dots: false,
+    infinite: false,
+
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  //----------------------------------------slider--------------------------------------//
   //----------------------------------------personal-menu--------------------------------------//
   $('.personal-menu__trigger').on('mouseover', function() {
     if ($(window).width() > 768) {
@@ -36,7 +74,7 @@ $(document).ready(function() {
   //----------------------------------------footer-menu--------------------------------------//
   //----------------------------------------menu-burger--------------------------------------//
   $('.menu-burger').on('click', function() {
-    $('.hidden-menu').show('400').css('display','flex');
+    $('.hidden-menu').show('400').css('display', 'flex');
   })
 
   $('.left-menu__close').on('click', function() {
@@ -49,19 +87,31 @@ $(document).ready(function() {
     $(this).addClass('active');
   })
   //----------------------------------------gender--------------------------------------//
-  //----------------------------------------slider--------------------------------------//
-  $('.brand-slider').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1
-  });
-//----------------------------------------slider--------------------------------------//
-//----------------------------------------green-button--------------------------------------//
+  //----------------------------------------green-button--------------------------------------//
   $('.banner__button').on('mouseenter', function() {
     setTimeout(() => { $('.banner__button').addClass('active'); }, 200);
   })
   $('.banner__button').on('mouseleave', function() {
     $('.banner__button').removeClass('active');
   })
-//----------------------------------------green-button--------------------------------------//
+  //----------------------------------------green-button--------------------------------------//
+  //----------------------------------------catalog-menu--------------------------------------//
+  $('.catalog-menu-trigger').on('click', function() {
+    event.preventDefault();
+    $('.menu-clother-list--hidden').show().addClass('active');
+  })
+
+  $('.catalog-menu-close').on('click', function() {
+    $('.menu-clother-list--hidden').removeClass('active');
+  })
+
+  $('.menu-clother__title').on('click', function() {
+    $(this).toggleClass('active');
+    $(this).next().toggle();
+    $(this).parent().hide().toggleClass('active').css('transform', 'translateX(120%)');
+    setTimeout(() => { $(this).parent().show().css('transform', 'translateX(0)'); }, 200);
+  })
+
+  //----------------------------------------catalog-menu--------------------------------------//
 
 });
